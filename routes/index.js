10c2c64b846369173345dd9b405f273a1d25b692
Karra <karra.maynard@gmail.com>
2024-01-14 17:20:23 -0500

    export and import index.js and notes.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const apiRouter = require('./notes');
+
 const app = express();
+
+app.use('/api', apiRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+// Import routes
+const api = require('./routes/index.js');
+
 const app = express();
 const PORT = 3001;
+
+app.use('/api', api);
 
 app.use(express.static('/public'));
 
