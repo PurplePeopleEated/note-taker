16e2d1b78fe1f5c9d7a4ca257861c9422b7e87a1
Karra <karra.maynard@gmail.com>
2024-01-24 22:29:33 -0500

    create post route

--- routes/notes.js
+++ routes/notes.js
@@ -8,4 +8,29 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// POST route add a note
+notes.post('/', (req, res) => {
+  // Destructure req.body
+  const {title, text} = req.body;
+  // Create a new note if there is a req.body
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      noteID: uuidv4()
+    };
+    // Add new note 
+    readAndAppend(newNote, './db/db.json');
+    // Respond to request
+    const response = {
+      status: 'success',
+      body: newNote
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in adding note');
+  }
+});
+
 module.exports = notes;
