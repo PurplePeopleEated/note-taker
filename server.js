989eef6f7de3e3828659c51e3cbbaf7f0338126c
Karra <karra.maynard@gmail.com>
2024-01-24 21:42:00 -0500

    move wildcard '/*' route below '/notes' get route

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(express.static('public'));
 app.use('/api', api);
 
 // HTML Routes
-app.get('/', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
