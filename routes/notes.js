7590d6736323243723d34f1d23d65a04ecaee150
Karra <karra.maynard@gmail.com>
2024-01-24 22:27:31 -0500

    import uuidv4 for random ids

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,6 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+// Import helper functions
 const {readFromFile, writeToFile, readAndAppend} = require('../helpers/utils');
 
 // GET route to get all notes
