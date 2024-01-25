4cbba6b3852260584ba3e1050741baf7ff75e8c7
Karra <karra.maynard@gmail.com>
2024-01-24 21:35:11 -0500

    remove '/' from express.static and add boilerplate

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const api = require('./routes/index.js');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api', api);
-
-app.use(express.static('/public'));
 
 // HTML Routes
 app.get('/', (req, res) => 
