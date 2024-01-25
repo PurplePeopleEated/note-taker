086123970e7292c801ca396038b2bf63dd318884
Karra <karra.maynard@gmail.com>
2024-01-24 21:51:24 -0500

    create get route for all saved notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,8 @@
 const notes = require('express').Router();
 
+// GET route to get all notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
 module.exports = notes;
