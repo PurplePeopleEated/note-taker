a22328aa7fed859b9c52f6ab223526252f15d142
Karra <karra.maynard@gmail.com>
2024-01-24 21:49:27 -0500

    add writeToFIle function

--- helpers/utils.js
+++ helpers/utils.js
@@ -3,4 +3,9 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 
+const writeToFile = (destination, content) =>
+fs.writeFile(destination, JSON.stringify(content), (err) =>
+  err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+
 module.exports = {readFromFile, writeToFile, readAndAppend};
